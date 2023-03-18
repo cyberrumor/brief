baf594d21f9698c1a7cdd14bcc17bb399c78d5ef
cyberrumor <cyberrumor@icloud.com>
2023-03-18 16:50:52 -0700

    Fix sentence splitting

--- brief.py
+++ brief.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
 import sys
 
-HONORIFICS = ["dr.", "mrs.", "mr.", "capt.", "prof.", "ms."]
-TERMINATIONS = ['!', '?', ")\"", '"', ")”", '”', ']']
+HONORIFICS = ["Dr.", "Mrs.", "Mr.", "Capt.", "Prof.", "Ms."]
+TERMINATIONS = ['!', '?', ")\"", '"', ")”", '”', ']', '.’']
 MAX_SENTENCE_LEN_CAP = 500
 
 class Corpus:
@@ -10,16 +10,18 @@ class Corpus:
         self.text = ""
         self.sentences = []
 
+        print(f"parsing: {file}")
         with open(file, "r") as f:
-            self.text = file.read()
+            self.text = f.read()
 
 
     def has_equal_pairs(self, sentence):
         if sentence.count('"') % 2 != 0:
             return False
 
-        if sentence.count('”') % 2 != 0:
-            return False
+        # unreliable
+        # if sentence.count('”') % 2 != 0:
+        #    return False
 
         if sentence.count('(') != sentence.count(')'):
             return False
@@ -27,13 +29,17 @@ class Corpus:
         if sentence.count('[') != sentence.count(']'):
             return False
 
+        # unreliable
+        # if sentence.count('‘') != sentence.count('’'):
+        #    return False
+
         return True
 
 
     def populate_sentences(self):
         sentence = ""
         sentences = []
-        tokens = self.corpus.split()
+        tokens = self.text.split()
         index = 0
         while index < len(tokens):
             # add a space if we have already started our sentence
@@ -42,6 +48,7 @@ class Corpus:
 
             # add the current word
             sentence += tokens[index]
+
             # prep for the next word
             index += 1
 
@@ -52,22 +59,23 @@ class Corpus:
                 # just throw the whole thing out so we don't parse forever.
                 # print something so we can try to prevent this thing in the future.
                 if len(sentence) > MAX_SENTENCE_LEN_CAP:
-                    print("we found a sentence that we were going to parse forever:")
-                    print(sentence)
+                    # print("we found a sentence that we were going to parse forever:")
+                    # print(sentence)
+                    # exit()
                     sentence = ""
                     continue
 
-                sentence_lower = sentence.lower()
-                if any([sentence_lower.endswith(i) for i in HONORIFICS]):
+                if any([sentence.endswith(i) for i in HONORIFICS]):
                     # it's an honorific, not a valid end of sentence.
                     continue
+
                 # If the last letter before the period was uppercase,
                 # it is probably a middle initial.
                 if len(sentence) > 1 and sentence[-2].isupper():
                     continue
 
                 # make sure we have even number of punctuations that require pairs.
-                if self.has_equal_pairs(sentence):
+                if not self.has_equal_pairs(sentence):
                     continue
 
                 # assume this period was a valid end of sentence.
@@ -77,7 +85,7 @@ class Corpus:
 
             # Identify sentences ending in valid punctuation, attention to
             # quotes used for dialogue.
-            if any([sentence.ends_with(i) for i in TERMINATIONS]):
+            if any([sentence.endswith(i) for i in TERMINATIONS]):
                 if self.has_equal_pairs(sentence):
                     # valid termination
                     sentences.append(sentence)
